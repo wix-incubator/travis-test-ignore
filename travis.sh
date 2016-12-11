91529a45ccfda4898cdad05d33881683a49a04b0
Leo Natan <leo.natan@outlook.com>
2016-12-11 14:03:58 +0200

    Add more stuff to script.

--- travis.sh
+++ travis.sh
@@ -3,5 +3,8 @@
 brew tap facebook/fb
 brew install fbsimctl --HEAD
 
-fbsimctl `fbsimctl list | grep Plus | head -n1 | sed 's/ \| .*//'` boot
-
+SIM=`fbsimctl list | grep Plus | head -n1 | sed 's/ \| .*//'`
+fbsimctl "$SIM" boot
+fbsimctl "$SIM" install LNPopupControllerExample.app
+fbsimctl "$SIM" list_apps
+fbsimctl "$SIM" launch com.LeoNatan.LNPopupControllerExample-
